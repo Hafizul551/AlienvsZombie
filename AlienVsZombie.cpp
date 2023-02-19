a7b4d27bcc4228387a64c61875a39ae10263d51f
Hafizul551 <124172453+Hafizul551@users.noreply.github.com>
2023-02-19 18:05:27 +0800

    Update AlienVsZombie.cpp

--- AlienVsZombie.cpp
+++ AlienVsZombie.cpp
@@ -13,6 +13,7 @@
 #include <string>
 #include <cstdlib>
 #include <ctime>
+#include <fstream>
 
 const int MAX_ROWS = 100;
 const int MAX_COLUMNS= 100;
@@ -20,7 +21,12 @@ const int MAX_COLUMNS= 100;
 int rows, columns, zombies, points;
 std::vector<std::vector<char>> gameBoard;
 int alienX, alienY;
+int alienHealth = 100;
+int zombieHealth = 30;
+int alienDamage = 30;
+int zombieDamage = 20;
 
+// Board function
 void intializeBoard()
 {
     gameBoard.resize(rows);
@@ -40,7 +46,7 @@ void intializeBoard()
         int y = rand() % columns;
         gameBoard[x][y] = 'Z';
     }
-    // Place alien ob board
+    // Place alien on board
     std::cout << "Enter alien starting position (x y): ";
     std::cin >> alienX >> alienY;
     gameBoard[alienX][alienY] = 'A';
@@ -52,13 +58,15 @@ void displayBoard()
     {
         for (int j = 0; j < columns; j++)
         {
-            std::cout << gameBoard[i][j] << '.';
+            std::cout << gameBoard[i][j] << ' ';
         }
         std::cout << '\n';
     }
     std::cout << "Points: " << points << '\n';
+    std::cout << "Alien Health: " << alienHealth << '\n';
 }
-// command control
+
+// movement control
 void moveAlien(const std::string &direction)
 {
     gameBoard[alienX][alienY] = '.';
@@ -90,10 +98,59 @@ void moveAlien(const std::string &direction)
             alienY--;
         }
     }
+    else
+    {
+        std::cout << "Invalid command" << std::endl;
+    }
+
+    // battle fight
     if (gameBoard[alienX][alienY] == 'Z')
     {
-        gameBoard[alienX][alienY] = '.';
-        points++;
+        alienHealth -= 20;
+        if (alienHealth <= 0)
+        {
+            std::cout << "The alien died!\n";
+            std::cout << "Game over!\n";
+            exit(0);
+        }
+    }
+
+    // check if alien and zombie are in the same position
+    if (gameBoard[alienX][alienY] == 'Z')
+    {
+        int zombieHealth = 30;
+        int alienHealth = 100;
+        int zombieDamage = 20;
+        int alienDamage = 30;
+        while (zombieHealth > 0 && alienHealth > 0)
+        {
+            //extra command whenever the alien meet the zombie
+            std::cout << "Press 'x' to attack the zombie: ";
+            std::string attack;
+            std::cin >> attack;
+
+            if (attack == "x")
+            {
+                zombieHealth -= alienDamage;
+                alienHealth -= zombieDamage;
+            }
+            else
+            {
+                std::cout << "invalid input, try again" << std::endl;
+            }
+        }
+        if (zombieHealth <= 0)
+        {
+            std::cout << "You have defeated the zombie!" << std::endl;
+            points++;
+            gameBoard[alienX][alienY] = 'A';
+        }
+        else if (alienHealth <= 0)
+        {
+            std::cout << "You have been defeated by the zombie!" << std::endl;
+            gameBoard[alienX][alienY] = '.';
+        }
+        
     }
     else
     {
@@ -101,14 +158,76 @@ void moveAlien(const std::string &direction)
     }
 }
 
-int main()
+// save and load function
+void saveGame(const std::string  &filename)
 {
-    std::cout << "Enter number of rows, columns, and zombies: ";
-    std::cin >> rows >> columns >> zombies;
-    intializeBoard();
-    displayBoard();
-    while (true)
+    std::ofstream outfile(filename);
+    if (!outfile)
     {
+        std::cerr << "Failed to open file" << filename << '\n';
+        return;
+    }
+    outfile << rows << ' ' << columns << ' ' << zombies << ' ' << points << ' ' << alienX << ' '  << alienY << ' ' << '\n';
+    for (int i = 0; i < rows; i++)
+    {   
+        for (int j = 0; j < columns; j++)
+        {   
+            outfile << gameBoard[i][j] << ' ';
+        }
+        outfile << '\n';
+    } 
+    outfile.close();
+}
+bool loadGame(const std::string &filename)
+{
+    std::ifstream infile(filename);
+    if (!infile)
+    {
+        std::cerr << "File not found: " << filename << '\n';
+        return false;
+    }
+    infile >> rows >> columns >> zombies >> points >> alienX >> alienY;
+    intializeBoard();
+
+    return true;
+}
+
+int main()
+{   std::cout << "Hello! Welcome to Alien vs Zombie" << std::endl;
+    std::cout << "Enter 'l' to load saved game or 'p' to start a new game: ";
+    std::string choice;
+    std::cin >> choice;
+
+    if (choice == "l")
+    {
+        std::string filename;
+        std::cout << "Enter the file name to load the game from: ";
+        std::cin >> filename;
+        if (loadGame(filename))
+        {
+            displayBoard();
+            std::cout << "You have " << points << "points.n";
+        }
+        else
+        {
+            std::cout << "No file detected.\n";
+            return 0;
+        }
+    }
+    else if (choice == "p")
+    {
+        std::cout << "Enter number of rows, columns and zombies: ";
+        std::cin >> rows >> columns >> zombies;
+        intializeBoard();
+        displayBoard();
+    }
+    else
+    {
+        std::cout << "Invalid input.\n";
+        return 0; 
+    }
+    while (true)
+    {   
         std::cout << "Enter direction ('h' for help or 'q' for quit): ";
         std::string direction;
         std::cin >> direction;
@@ -118,6 +237,17 @@ int main()
         }
         else if (direction == "q")
         {
+            std::cout << "Enter 's' to save the game or 'q' to quit without saving: ";
+            std::string saveChoice;
+            std::cin >> saveChoice;
+            if (saveChoice == "s")
+            {
+                std::string filename;
+                std::cout << "Enter the file name: ";
+                std::cin >> filename;
+                saveGame (filename);
+                std::cout << "Game saved!\n";
+            }
             std::cout << "Thank you for playing!\n";
             break;
         }
@@ -129,7 +259,7 @@ int main()
                 points++;
             }
             displayBoard();
-            std::cout << "You have" << points << " points.\n";
+            std::cout << "You have " << points << " points.\n";
         }
         if (points == zombies)
         {
